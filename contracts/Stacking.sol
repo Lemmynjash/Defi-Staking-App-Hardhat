80f394b218f20e5b9f33af9524793e6182e55332
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-05-03 22:11:35 +0300

    guidelines

--- contracts/Stacking.sol
+++ contracts/Stacking.sol
@@ -6,7 +6,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
-contract Stacking{
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+contract Stacking {
+    IERC20 public s_stackingToken;
 
+    constructor(address stackingToken) {
+        s_stackingToken = IERC20(stackingToken);
+    }
+
+    function stake(uint256 amount) external {
+
+        //keep track of how much stake user A has
+        //keep track of how much token we have
+        //transfer the tokens to the contract address which will be deployed to
+        
+         
+    }
 }
