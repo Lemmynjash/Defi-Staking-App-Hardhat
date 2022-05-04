1e67486fad8b2c9e45813fd8ea4be320cf84e8c2
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-05-04 09:20:36 +0300

    working on the staking contract

--- contracts/Stacking.sol
+++ contracts/Stacking.sol
@@ -10,17 +10,28 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Stacking {
     IERC20 public s_stackingToken;
+    uint256 public s_totalSupply;
+    //mapping someones address to ----> what someone has staked
+    mapping(address => uint256) public s_balances;
 
     constructor(address stackingToken) {
         s_stackingToken = IERC20(stackingToken);
     }
 
     function stake(uint256 amount) external {
+        //STEP 1: keep track of how much stake user A has
+        //STEP 2: keep track of how much token we have
+        //STEP 3: transfer the tokens to the contract address which will be deployed to
 
-        //keep track of how much stake user A has
-        //keep track of how much token we have
-        //transfer the tokens to the contract address which will be deployed to
-        
-         
+        //STEP 1:
+        s_balances[msg.sender] = s_balances[msg.sender] + amount;
+        //STEP 2:
+        s_totalSupply = s_totalSupply + amount;
+        //STEP 3
+        bool success = s_stackingToken.transfer(
+            msg.sender,
+            address(this),
+            amount
+        );
     }
 }
